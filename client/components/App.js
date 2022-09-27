a51a323384a3d3d16305808009f69c19965e14f9
Krisette Odegard <kmodeg@gmail.com>
2022-09-27 15:23:22 -0700

    added extremely basic front end functionality: fetching trips, adding a trip, updating a trip, deleting a trip

--- client/components/App.js
+++ client/components/App.js
@@ -1,13 +1,45 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import NewTrip from './NewTrip';
+import UpdateTrip from './UpdateTrip';
+import DeleteTrip from './DeleteTrip';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Imagine stuff</h1>
+const App = () => {
+  const [trips, setTrips] = useState(null);
+
+  useEffect(() => {
+    const getTrips = () => {
+      fetch('/trips')
+        .then(res => res.json())
+        .then(data => {
+          setTrips(data);
+        })
+        .catch(err => console.log(err));
+    }
+    getTrips();
+  }, []);
+
+  return (
+    <div className="main">
+      <h1>iMaGiNaShUN</h1>
+      <div className="Trips">
+        <h2>All Trips</h2>
+          {trips && trips.map(trip => {
+            return (
+              <div className="trip">
+                <p>{trip.start_date}</p>
+                <p>{trip.end_date}</p>
+                <p>{trip.hotel}</p>
+                <p>{trip.parks}</p>
+              </div>
+            )
+          })}
+        </div>
+        <NewTrip />
+        <UpdateTrip />
+        <DeleteTrip />
       </div>
-    );
-  }
+  )
 }
 
 export default App;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-const tripRouter = require(path.join(__dirname, '/routes/trips.js'));
 const mongoose = require('mongoose');
+// for storing secrets
 require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 
 // start express app!
@@ -15,6 +15,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
 
+// declare route handlers
+const tripRouter = require(path.join(__dirname, '/routes/trips.js'));
+
 // route handler for trips
 app.use('/trips', tripRouter);
 
@@ -22,14 +25,13 @@ app.use('/trips', tripRouter);
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     app.listen(process.env.PORT, () => {
-      console.log('db connected & server listening on port 3000');
+      console.log(`db connected & server listening on port ${process.env.PORT}`);
     });
   })
   .catch((err) => {
     console.log('error connecting to db', err);
   });
   
-
 // error handler for unknown route
 app.use((req, res) => res.status(404).send("404: No magic here!"));
 
