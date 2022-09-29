9c2ff721e1b6e621138e58b345a30624e4263dcf
Krisette Odegard <kmodeg@gmail.com>
2022-09-29 00:58:31 -0700

    added fake logout, although /logout route is defined in backend

--- client/components/NavBar.js
+++ client/components/NavBar.js
@@ -2,12 +2,17 @@ import React from 'react';
 import "../stylesheets/_navbar.scss";
 import { Link } from "react-router-dom";
 export default function NavBar( {user} ) {
+
+  const logOut = () => {
+    window.location.reload();
+  }
+
   return (
 		<header className='navbar'>
         <div className='navbar__title navbar__item'>welcome, {user}!</div>
         <div className='navbar__item'>all trips</div>
         <div className='navbar__item'>current wait times</div>
-        <div className='navbar__item'>log out</div>        
+        <div className='navbar__item'><a onClick={logOut}>log out</a></div>        
     </header>
   );
 }
